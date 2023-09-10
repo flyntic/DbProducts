f57f7933dd247d56da6993cdb733038e07eef393
flyntic <64376713+flyntic@users.noreply.github.com>
2023-09-10 23:48:25 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Model;
+
+namespace WebApplication3.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ WebApplication3/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication3.Model
+{
+    public class Product
+    {
+        public int Id;
+        public string Article;
+        public string Name;
+        public string Brand;
+        public string Type;
+        public decimal Price;
+        public decimal Weight;
+        public int Count;
+
+    }
+}
